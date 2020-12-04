54eec36b2af8cd19e38ad7738faa82716530023f
Cédric Morier-Roy <cmorierroy@gmail.com>
2020-12-03 18:36:51 -0600

    feat: slightly cleaned up UI of DetailsVC - added title for ward and corrected background and legend colours

--- WinnipegElectionResults/Controller/DetailVC/DetailVC.swift
+++ WinnipegElectionResults/Controller/DetailVC/DetailVC.swift
@@ -21,6 +21,27 @@ class DetailVC: UIViewController {
         super.viewDidLoad()
         
         loadPieChart()
+        
+        //set up title label
+        let title = UILabel()
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.text = chartTitle
+        
+        var constraints = [NSLayoutConstraint]()
+        
+        //Add
+        //constraints.append(title.leadingAnchor.constraint(equalTo:view.safeAreaLayoutGuide.leadingAnchor))
+        //constraints.append(title.trailingAnchor.constraint(equalTo:view.safeAreaLayoutGuide.trailingAnchor))
+        //constraints.append(title.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor))
+        //constraints.append(title.bottomAnchor.constraint(equalTo:view.safeAreaLayoutGuide.bottomAnchor))
+
+        //Activate
+        //NSLayoutConstraint.activate(constraints)
+        
+        //title.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        //title.topAnchor.constraint(equalTo: view.topAnchor, constant: 20)
+        
+        pieChart.addSubview(title)
         pieChart.animate(xAxisDuration: 1, yAxisDuration: 1)
     }
     
@@ -52,20 +73,22 @@ class DetailVC: UIViewController {
         //Set ChartData
         let pieChartData = PieChartData(dataSet: pieChartDataSet)
         let format = NumberFormatter()
-        format.numberStyle = .none
+        format.numberStyle = .decimal
         let formatter = DefaultValueFormatter(formatter: format)
         pieChartData.setValueFormatter(formatter)
+        pieChartData.setValueTextColor(.black)
         
         //Assign it to the chart’s data
         
         //CUSTOMIZATION OF PIE CHART
-        pieChart.largeContentTitle = chartTitle
-        pieChart.holeColor = .black
+        pieChart.entryLabelColor = .black
+        pieChart.centerText = "Democratic score:..."
+        pieChart.holeColor = UIColor.AppTheme.paleYellow
         pieChart.data = pieChartData
-        pieChart.backgroundColor = .black
+        pieChart.backgroundColor = UIColor.AppTheme.paleYellow
         
         //customize legend
-        pieChart.legend.textColor = .white
+        pieChart.legend.textColor = .black
         
     }
     
---
+++ WinnipegElectionResults/Model/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  WinnipegElectionResults
+//
+//  Created by Cédric Morier-Roy on 2020-12-03.
+//
+
+import UIKit
+
+extension UIColor
+{
+    struct AppTheme
+    {
+        static var paleYellow: UIColor  { return UIColor(red: 254.0/255, green: 244.0/255, blue: 157.0/255, alpha: 1) }
+    }
+}
